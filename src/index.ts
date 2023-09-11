a9a2192ec5ea8141c4410227ab9ca945a9d021e5
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-09-11 09:24:16 -0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
 
-const port = 3002;
+mongoose.connect("mongodb://localhost:27017")
+	.then(() => {
+		const app = express();
+		const port = 3001;
 
-app.listen(port, () => {
-	console.log("Server isrunning on https://localhost:3001");
-});
+		app.listen(port, () => {
+			console.log(`Server isrunning on https://localhost:${port}`);
+		});
+	})
+	.catch(() => console.log("Erro ao conectar com o mongoDB"));
+
