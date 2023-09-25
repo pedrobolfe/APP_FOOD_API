f184c8bd0fa2cdadf0d20d7c44536ed0947d41d5
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-09-25 09:12:47 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,19 @@ import mongoose from 'mongoose';
 
 import { router } from './router';
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb://localhost:27017') // conecta com o banco de dados
 	.then(() => {
-		const app = express();
-		const port = 3000;
 
+		const app = express(); // 
+		const port = 3000; // porta 
+
+		// configurando o express
 		app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-		app.use(express.json());
-		app.use(router);
+		app.use(express.json()); // sempre que uma solicitação chegar no back-end, o Express executará as funções
+		app.use(router); // add uma nova funcao ao app
 		//teclado de emotion tecla win+.
 		app.listen(port, () => {
-			console.log(`🚗Server is runing on http://localhost:${port}`);
+			console.log(`🚗Server is runing on http://localhost:${port}`); // servidor rodando e link do server
 		});
 	})
-	.catch(() => console.log('Erro ao conectar no mongoDb'));
+	.catch(() => console.log('Erro ao conectar no mongoDb')); // msg de erro
